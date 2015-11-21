921c7a457b4475eb7a247035516e8e48e432ff58
Hugo <hugo.polsinelli@epfl.ch>
2015-11-21 23:00:13 +0100

    +Fix

---
+++ src/ch/epfl/maze/physical/Maze.java
@@ -0,0 +1,97 @@
+package ch.epfl.maze.physical;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Maze in which an animal starts from a starting point and must find the exit.
+ * Every animal added will have its position set to the starting point. The
+ * animal is removed from the maze when it finds the exit.
+ * 
+ */
+
+public final class Maze extends World {
+
+    private List<Animal> _animalsIn = new ArrayList<Animal>();
+    private List<Animal> _animalsOut = new ArrayList<Animal>();
+
+    /**
+     * Constructs a Maze with a labyrinth structure.
+     * 
+     * @param labyrinth
+     *            Structure of the labyrinth, an NxM array of tiles
+     */
+
+    public Maze(int[][] labyrinth) {
+	super(labyrinth);
+    }
+
+    @Override
+    public boolean isSolved() {
+	return _animalsIn.isEmpty();
+    }
+
+    @Override
+    public List<Animal> getAnimals() {
+	return _animalsIn;
+    }
+
+    /**
+     * Determines if the maze contains an animal.
+     * 
+     * @param a
+     *            The animal in question
+     * @return <b>true</b> if the animal belongs to the world, <b>false</b>
+     *         otherwise.
+     */
+
+    public boolean hasAnimal(Animal a) {
+	for (Animal an : _animalsIn)
+	    if (an == a)
+		return true;
+
+	return false;
+    }
+
+    /**
+     * Adds an animal to the maze.
+     * 
+     * @param a
+     *            The animal to add
+     */
+
+    public void addAnimal(Animal a) {
+	a.setPosition(getStart());
+	_animalsIn.add(a);
+    }
+
+    /**
+     * Removes an animal from the maze.
+     * 
+     * @param a
+     *            The animal to remove
+     */
+
+    public void removeAnimal(Animal a) {
+	int index = -1;
+	_animalsOut.add(a.copy());
+	for (int i = 0; i < _animalsIn.size(); i++)
+	    if (_animalsIn.get(i) == a)
+		index = i;
+
+	if (index >= 0)
+	    _animalsIn.remove(index);
+    }
+
+    @Override
+    public void reset() {
+/*	for (Animal a : _animalsIn)
+	    _animalsOut.add(a.copy());
+
+	_animalsIn.clear();
+	for (Animal a : _animalsOut)
+	    addAnimal(a);
+
+	_animalsOut.clear();*/
+    }
+}
---
+++ src/ch/epfl/maze/physical/World.java
@@ -0,0 +1,184 @@
+package ch.epfl.maze.physical;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.epfl.maze.util.Direction;
+import ch.epfl.maze.util.Vector2D;
+
+/**
+ * World that is represented by a labyrinth of tiles in which an {@code Animal}
+ * can move.
+ * 
+ */
+
+public abstract class World {
+
+    /* tiles constants */
+    public static final int FREE = 0;
+    public static final int WALL = 1;
+    public static final int START = 2;
+    public static final int EXIT = 3;
+    public static final int NOTHING = -1;
+    public static final Direction[] DIRECTIONS = { Direction.UP, Direction.DOWN, Direction.RIGHT, Direction.LEFT, };
+    final protected Vector2D INVALID_POS = new Vector2D(-1, -1);
+
+    private int[][] _labyrinth;
+
+    /**
+     * Constructs a new world with a labyrinth. The labyrinth must be rectangle.
+     * 
+     * @param labyrinth
+     *            Structure of the labyrinth, an NxM array of tiles
+     */
+
+    public World(int[][] labyrinth) {
+	_labyrinth = labyrinth;
+    }
+
+    /**
+     * Determines whether the labyrinth has been solved by every animal.
+     * 
+     * @return <b>true</b> if no more moves can be made, <b>false</b> otherwise
+     */
+
+    abstract public boolean isSolved();
+
+    /**
+     * Resets the world as when it was instantiated.
+     */
+
+    abstract public void reset();
+
+    /**
+     * Returns a copy of the list of all current animals in the world.
+     * 
+     * @return A list of all animals in the world
+     */
+
+    abstract public List<Animal> getAnimals();
+
+    /**
+     * Checks in a safe way the tile number at position (x, y) in the labyrinth.
+     * 
+     * @param x
+     *            Horizontal coordinate
+     * @param y
+     *            Vertical coordinate
+     * @return The tile number at position (x, y), or the NONE tile if x or y is
+     *         incorrect.
+     */
+
+    public final int getTile(int x, int y) {
+	if ((x >= _labyrinth[0].length) || (y >= _labyrinth.length))
+	    return World.NOTHING;
+	else
+	    return _labyrinth[y][x];
+    }
+
+    /**
+     * Determines if coordinates are free to walk on.
+     * 
+     * @param x
+     *            Horizontal coordinate
+     * @param y
+     *            Vertical coordinate
+     * @return <b>true</b> if an animal can walk on tile, <b>false</b> otherwise
+     */
+
+    public final boolean isFree(int x, int y) {
+	if ((getTile(x, y) == World.NOTHING) || (getTile(x, y) == World.WALL))
+	    return false;
+	else
+	    return true;
+    }
+
+    /**
+     * Determines if the coordinates are free to walk on.
+     * 
+     * @param pos
+     *            Vector2D of the coordinates
+     * @return <b>true</b> if an animal can walk on tile, <b>false</b> otherwise
+     */
+
+    public final boolean isFree(Vector2D pos) {
+	return isFree(pos.getX(), pos.getY());
+    }
+
+    /**
+     * Computes and returns the available choices for a position in the
+     * labyrinth. The result will be typically used by {@code Animal} in
+     * {@link ch.epfl.maze.physical.Animal#move(Direction[]) move(Direction[])}
+     * 
+     * @param position
+     *            A position in the maze
+     * @return An array of all available choices at a position
+     */
+
+    public final Direction[] getChoices(Vector2D position) {
+	List<Direction> avaliableDirections = new ArrayList<Direction>();
+	for (Direction dir : DIRECTIONS)
+	    if (isFree(position.addDirectionTo(dir)))
+		avaliableDirections.add(dir);
+
+	if (avaliableDirections.isEmpty())
+	    avaliableDirections.add(Direction.NONE);
+
+	Direction[] returnArray = new Direction[avaliableDirections.size()];
+	for (int i = 0; i < avaliableDirections.size(); i++)
+	    returnArray[i] = avaliableDirections.get(i);
+
+	return returnArray;
+    }
+
+    /**
+     * Returns horizontal length of labyrinth.
+     * 
+     * @return The horizontal length of the labyrinth
+     */
+
+    public final int getWidth() {
+	return _labyrinth[0].length;
+    }
+
+    /**
+     * Returns vertical length of labyrinth.
+     * 
+     * @return The vertical length of the labyrinth
+     */
+
+    public final int getHeight() {
+	return _labyrinth.length;
+    }
+
+    /**
+     * Returns the entrance of the labyrinth at which animals should begin when
+     * added.
+     * 
+     * @return Start position of the labyrinth, null if none.
+     */
+
+    public final Vector2D getStart() {
+	for (int i = 0; i < _labyrinth.length; i++)
+	    for (int j = 0; j < _labyrinth[i].length; j++)
+		if (_labyrinth[i][j] == World.START)
+		    return new Vector2D(j, i);
+
+	return null;
+    }
+
+    /**
+     * Returns the exit of the labyrinth at which animals should be removed.
+     * 
+     * @return Exit position of the labyrinth, null if none.
+     */
+
+    public final Vector2D getExit() {
+	for (int i = 0; i < _labyrinth.length; i++)
+	    for (int j = 0; j < _labyrinth[i].length; j++)
+		if (_labyrinth[i][j] == World.EXIT)
+		    return new Vector2D(j, i);
+
+	return null;
+    }
+}
